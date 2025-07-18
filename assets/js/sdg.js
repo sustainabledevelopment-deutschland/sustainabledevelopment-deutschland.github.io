1798cbb88168b2d2cccbce57a6b7044c0e69917d
MoBosse <50657629+MoBosse@users.noreply.github.com>
2025-07-18 14:29:00 +0000

    Deploying to gh-pages from  @ 24a121d2d13c5dd81615526cfb704739f6fe3ff5 🚀

--- assets/js/sdg.js
+++ assets/js/sdg.js
@@ -4219,7 +4219,7 @@ opensdg.chartTypes.base = function(info) {
                         label: function (tooltipItem) {
 
                           var label =  translations.t(tooltipItem.dataset.label);
-                          label = label.replace('<sub>','').replace('</sub>','');
+                          label = label.replace('<sub>','').replace('</sub>','').replace("<u>","").replace("</u>","");
                           if (label.length > 45){
 
                             label = label.split(' ');
@@ -4241,7 +4241,7 @@ opensdg.chartTypes.base = function(info) {
                         afterLabel: function(tooltipItem) {
 
                           var label =  tooltipItem.dataset.label;
-                          label = label.replace('<sub>','').replace('</sub>','');
+                          label = label.replace('<sub>','').replace('</sub>','').replace("<u>","").replace("</u>","");
                           if (label.length > 45){
                             label = label.split(' ');
                             var re = [];
